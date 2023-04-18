0d27cb3ceafac309234a779cc2d9b26dda500886
LachezarIvanovv <lachezar.ivanov.0888@abv.bg>
2023-04-18 14:55:21 +0300

    Optimization

--- src/main/java/softuni/Mobilele/model/service/UserService.java
+++ src/main/java/softuni/Mobilele/model/service/UserService.java
@@ -2,8 +2,6 @@ package softuni.Mobilele.model.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import softuni.Mobilele.model.dto.UserLoginDTO;
