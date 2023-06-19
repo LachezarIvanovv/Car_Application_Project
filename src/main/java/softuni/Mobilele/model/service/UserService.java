3000d311c32c3b26b6f988e6eea22adebb3b899b
LachezarIvanovv <lachezar.ivanov.0888@abv.bg>
2023-06-19 10:57:34 +0300

    Next step towards Spring Security

--- src/main/java/softuni/Mobilele/model/service/UserService.java
+++ src/main/java/softuni/Mobilele/model/service/UserService.java
@@ -1,37 +1,24 @@
 package softuni.Mobilele.model.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import softuni.Mobilele.model.dto.UserLoginDTO;
 import softuni.Mobilele.model.dto.UserRegisterDTO;
 import softuni.Mobilele.model.entity.UserEntity;
 import softuni.Mobilele.model.mapper.UserMapper;
 import softuni.Mobilele.repository.UserRepository;
-import softuni.Mobilele.user.CurrentUser;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
 
-    private Logger LOGGER = LoggerFactory.getLogger(UserService.class);
-
     private final UserRepository userRepository;
-    private final CurrentUser currentUser;
     private final PasswordEncoder passwordEncoder;
     private final UserMapper userMapper;
 
 
     public UserService(UserRepository userRepository,
-                       CurrentUser currentUser,
                        PasswordEncoder passwordEncoder,
                        UserMapper userMapper){
         this.userRepository = userRepository;
-        this.currentUser = currentUser;
         this.passwordEncoder = passwordEncoder;
         this.userMapper = userMapper;
     }
@@ -45,36 +32,9 @@ public class UserService {
 
     }
 
-    public boolean login(UserLoginDTO loginDTO){
-        Optional<UserEntity> userOpt = userRepository.findByEmail(loginDTO.getUsername());
-
-        if(userOpt.isEmpty()){
-            LOGGER.info("User with name [{}] not found.", loginDTO.getUsername());
-            return false;
-        }
-
-        String rawPassword = loginDTO.getPassword();
-        String encodedPassword = userOpt.get().getPassword();
-
-        boolean success = passwordEncoder.matches(rawPassword, encodedPassword);
-
-        if(success){
-            login(userOpt.get());
-        } else {
-            logout();
-        }
-
-        return success;
-    }
 
     private void login(UserEntity userEntity){
-        currentUser
-                .setLoggedIn(true)
-                .setName(userEntity.getFirstName() + " " + userEntity.getLastName())
-                .setEmail(userEntity.getEmail());
+        //todo
     }
 
-    public void logout(){
-        currentUser.clear();
-    }
 }
--- src/main/java/softuni/Mobilele/web/UserController.java
+++ src/main/java/softuni/Mobilele/web/UserController.java
@@ -1,41 +1,17 @@
 package softuni.Mobilele.web;
 
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import softuni.Mobilele.model.dto.UserLoginDTO;
-import softuni.Mobilele.model.dto.UserRegisterDTO;
-import softuni.Mobilele.model.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/login")
     public String login(){
         return "auth-login";
     }
-
-    @GetMapping("/logout")
-    public String logout(){
-        userService.logout();
-        return "redirect:/";
-    }
-
-    @PostMapping("/login")
-    public String login(UserLoginDTO userLoginDTO){
-        userService.login(userLoginDTO);
-        return "redirect:/";
-    }
-
 
 }
